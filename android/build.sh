13486da5031b616df859c3eb4c105ec637707530
Egist Li <egistli@invisibi.com>
2015-01-20 15:21:43 +0800

    set PROJECT_ROOT to env $CUSTOM_WEBRTC_ROOT

--- android/build.sh
+++ android/build.sh
@@ -7,20 +7,7 @@
 # Builds the android peer connection library
 
 # Set your environment how you want
-if [ -n  "$VAGRANT_MACHINE" ];
-    then
-    PROJECT_ROOT="/vagrant"
-else
-    # Get location of the script itself .. thanks SO ! http://stackoverflow.com/a/246128
-    SOURCE="${BASH_SOURCE[0]}"
-    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-        SOURCE="$(readlink "$SOURCE")"
-        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    done
-    PROJECT_ROOT="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-fi
-
+PROJECT_ROOT="$CUSTOM_WEBRTC_ROOT"
 DEPOT_TOOLS="$PROJECT_ROOT/depot_tools"
 WEBRTC_ROOT="$PROJECT_ROOT/webrtc"
 BUILD="$WEBRTC_ROOT/libjingle_peerconnection_builds"
